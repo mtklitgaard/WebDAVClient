e6a4add2f0fda09c12a7a7a622b3348a1686d3c1
Matt Klitgaard <mklitgaa@1800contacts.com>
2015-05-20 09:01:11 -0600

    Add file upload

---
+++ FileUploaderTests/UploadTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace FileUploaderTests
+{
+    [TestFixture]
+    public class UploadTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            
+        }
+
+        public class Upload : UploadTests
+        {
+            [Test]
+            public void CallsGetSubDirectories_FromListDirectories()
+            {
+                
+            }
+        }
+    }
+}
