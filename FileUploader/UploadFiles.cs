d325a96810aa734b8dfb485783f6b21bfe440ba1
mtklitgaard <mtklitgaard@gmail.com>
2015-06-12 11:18:27 -0600

    add missing files

---
+++ FileUploader/UploadFiles.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FileUploader.Interfaces;
+using WebDAVClient.Interfaces;
+
+namespace FileUploader
+{
+    public class UploadFiles
+    {
+        private readonly IDirectoryUtilityWrapper _directoryUtilityWrapper;
+        private readonly IWebDAVOperator _webDavOperator;
+        private readonly IFileCreator _fileCreator;
+
+        public UploadFiles(IDirectoryUtilityWrapper directoryUtilityWrapper, IWebDAVOperator webDavOperator, IFileCreator fileCreator)
+        {
+            _directoryUtilityWrapper = directoryUtilityWrapper;
+            _webDavOperator = webDavOperator;
+            _fileCreator = fileCreator;
+        }
+
+
+        public async void Upload(string pathToRootDirectory)
+        {
+            var uploadRootDirectory = FindRootDirectory(pathToRootDirectory);
+            var directories = _directoryUtilityWrapper.GetTopDirectories(pathToRootDirectory);
+            var rootPath = "/";
+            
+            await _webDavOperator.CreateDir(rootPath, uploadRootDirectory);
+
+            CreateDirectories(directories);
+        }
+
+        private async void CreateDirectories(List<string> directories)
+        {
+            foreach (var directory in directories)
+            {
+                var flattenedDirectories = FlattenDirectoryStructure(directory);
+                var folderLocation = flattenedDirectories.Take(flattenedDirectories.Count - 1).ToList();
+                var directoryLocation = string.Join("\\", folderLocation);
+
+                await _webDavOperator.CreateDir(directoryLocation + "\\", flattenedDirectories.Last());
+
+                var subDirectories = _directoryUtilityWrapper.GetTopDirectories(directory);
+                if (subDirectories != null && subDirectories.Count > 0)
+                {
+                    CreateDirectories(subDirectories);
+                }
+
+                _fileCreator.Upload(directory, directoryLocation + "\\" + flattenedDirectories.Last());
+            }
+        }
+
+        private List<string> FlattenDirectoryStructure(string directory)
+        {
+            var directories = directory.Split('\\').ToList();
+
+            return directories.Where(x => !x.Contains(':')).ToList();
+        }
+
+        private string FindRootDirectory(string pathToRootDirectory)
+        {
+            var path = pathToRootDirectory.Split('\\').ToList();
+
+            return path.First(x => !x.Contains(':'));
+        }
+    }
+}
---
+++ FileUploaderTests/FileCreatorTests.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using FileUploader;
+using FileUploader.Interfaces;
+using Moq;
+using NUnit.Framework;
+using WebDAVClient.Interfaces;
+
+namespace FileUploaderTests
+{
+    public class FileCreatorTests
+    {
+        private Mock<IWebDAVOperator> _webDAVOperator;
+        private Mock<IDirectoryUtilityWrapper> _directoryUtilityWrapper;
+        private FileCreator _classUnderTest;
+
+        [SetUp]
+        public void Setup()
+        {
+            _webDAVOperator = new Mock<IWebDAVOperator>();
+            _directoryUtilityWrapper = new Mock<IDirectoryUtilityWrapper>();
+            _classUnderTest = new FileCreator(_webDAVOperator.Object, _directoryUtilityWrapper.Object);
+        }
+
+        public class Upload : FileCreatorTests
+        {
+            [Test]
+            public void CallsGetFilesAndCallsUploadFiles_WhenFilesExist()
+            {
+                var directoryName = "test";
+                var fileLocation = "file1";
+                var files = new List<string>
+                {
+                    fileLocation
+                };
+
+                _directoryUtilityWrapper.Setup(x => x.GetAllFiles(directoryName)).Returns(files);
+                
+                _classUnderTest.Upload(directoryName, "test");
+
+                _directoryUtilityWrapper.Verify(x => x.GetAllFiles(directoryName));
+                _webDAVOperator.Verify(x => x.Upload(It.IsAny<string>(), It.IsAny<Stream>(), It.IsAny<string>()));
+            }
+        }
+    }
+}
